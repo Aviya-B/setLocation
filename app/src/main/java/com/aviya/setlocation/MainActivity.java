7c9deeece188ca4a0ce9db9f6ac6da5b4860be17
chemda <chemda96@gmail.com>
2018-06-20 23:23:47 +0300

    commit

--- app/src/main/java/com/aviya/setlocation/MainActivity.java
+++ app/src/main/java/com/aviya/setlocation/MainActivity.java
@@ -1,36 +1,103 @@
 package com.aviya.setlocation;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
-    private Button btnChoose;
+ private Button btnAdd;
+    private ArrayList<Place> arrayList;
+    private ArrayAdapter<Place> adapter;
+    private ListView listView;
+
+    private Location location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_location);
+        btnAdd =  findViewById(R.id.btnAddID);
+        btnAdd.setOnClickListener(this);
+        arrayList = new ArrayList<>();
+        location = new Location("location");
+        Place place = new Place("location1",location,20,false,false);
+        arrayList.add(place);
 
+        adapter = new ArrayAdapter<Place>(this,android.R.layout.simple_list_item_1,arrayList);
 
-        btnChoose =  findViewById(R.id.btnChooseID);
+        listView = (ListView) findViewById(R.id.myListViewID);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l)
+            {
+                Intent t = new Intent(MainActivity.this,SettingActivity.class);
+                t.putExtra("index",index);
+                startActivity(t);
+                //Toast.makeText(MainActivity.this, arrayList.get(index),Toast.LENGTH_LONG).show();
+            }
+        });
 
-
-        btnChoose.setOnClickListener(this);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int index, long l)
+            {
+                arrayList.remove(index);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
-
 
     @Override
     public void onClick(View v) {
+        if(v==btnAdd) {
 
-        if(v==btnChoose) {
-            Intent t = new Intent(this,LocationActivity.class);
+            Intent t = new Intent(MainActivity.this, MapActivity.class);
             startActivity(t);
         }
+    }
 
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        // if service is on
+        menu.clear();
+        menu.add(0,1,Menu.NONE,"Sign out");
+        // if service is off
+        menu.clear();
+        menu.add(0,2,Menu.NONE,"Sign in");
+        return super.onPrepareOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case 1:
+                Toast.makeText(this, "Sign out", Toast.LENGTH_SHORT).show();
+                finish();
+            case 2:
+                Toast.makeText(this, "Sign in", Toast.LENGTH_SHORT).show();
+                break;
+        }
 
-
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
--- app/src/main/java/com/aviya/setlocation/Place.java
+++ app/src/main/java/com/aviya/setlocation/Place.java
@@ -1,4 +1,71 @@
-public class Places {
+package com.aviya.setlocation;
+
+import android.app.Notification;
+import android.location.Location;
+
+public class Place
+{
     private String name;
-    
+    private Location location;
+    private double radius;
+    private boolean isSilent;
+    private String msg;
+    private boolean ifRecord;
+
+    public Place(String name,Location location, double radius,boolean isSilent,boolean ifRecord){
+        this.name= name;
+        this.location=location;
+        this.radius=radius;
+        this.isSilent=isSilent;
+        this.msg="message";
+        this.ifRecord=ifRecord;
+}
+
+    public boolean isIfRecord() {
+        return ifRecord;
+    }
+
+    public void setIfRecord(boolean ifRecord) {
+        this.ifRecord = ifRecord;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public boolean isSilent() {
+        return isSilent;
+    }
+
+    public void setSilent(boolean silent) {
+        isSilent = silent;
+    }
 }
